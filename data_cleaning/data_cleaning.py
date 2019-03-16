2013aca2375a64ccda08f04e833c0a590c5ba0ca
claudiopacha <claudiomonteiro@protonmail.com>
2019-03-16 17:35:30 -0300

    process init

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -8,30 +8,43 @@ claudioalvesmonteiro.github.io
 
 # importar pacotes
 import pandas as pd
+import unidecode
 
+# unaccented_string contains 'Malaga'and is of type 'str
 #===== importar dados =====#
-
+"""
 # SNIS
-dataSNIS = pd.ExcelFile('original_data/SNIS_serie_Recife.xlsx')
-dataSNIS = dataSNIS.parse(0)
+dataSNIS = pd.ExcelFile('original_data/SNIS_serie_Recife.xlsx').parse(0)
 
 # Plantas Agamenom
-dataArbAga = pd.ExcelFile('original_data/plantas_agamenom.xls')
-dataArbAga = dataArbAga.parse(0)
+dataArbAga = pd.ExcelFile('original_data/plantas_agamenom.xls').parse(0)
 
 # IBGE Censos 2000 e 2010
-ibgeAgua2000 = pd.ExcelFile('original_data/IBGE/abastecimento_agua-CENSO2000.xls')
-ibgeAgua2000 = ibgeAgua2000.parse(0)
+ibgeAgua2000 = pd.ExcelFile('original_data/IBGE/abastecimento_agua-CENSO2000.xls').parse(0)
+ibgeLixo2000 = pd.ExcelFile('original_data/IBGE/destino_lixo-CENSO2000.xls').parse(0)
+ibgeAguaLixo2010 = pd.ExcelFile('original_data/IBGE/abastecimento_agua-destino_lixo-CENSO2010.xls').parse(0)
+ibgeEsgoto2000 = pd.ExcelFile('original_data/IBGE/esgotamento-CENSO2000.xls').parse(0)
+ibgeEsgoto2010 = pd.ExcelFile('original_data/IBGE/esgotamento-CENSO2010.xls').parse(0)
+"""
+#===========================#
+# Padronizacao
+#===========================#
 
-ibgeLixo2000 = pd.ExcelFile('original_data/IBGE/destino_lixo-CENSO2000.xls')
-ibgeLixo2000 = ibgeLixo2000.parse(0)
+# funcao para padronizar nomes das variaveis
+def standVarName(frase):
+    std_string = ""
+    for caracter in frase:
+        if caracter == " ":
+            std_string += "_"
+        else:
+            std_string += unidecode.unidecode(caracter)
+    return std_string.lower()
+    
+test = "Caçamba de serviço de limpeza"
+print(standVarName(test))
 
-ibgeAguaLixo2010 = pd.ExcelFile('original_data/IBGE/abastecimento_agua-destino_lixo-CENSO2010.xls')
-ibgeAguaLixo2010 = ibgeAguaLixo2010.parse(0)
 
-ibgeEsgoto2000 = pd.ExcelFile('original_data/IBGE/esgotamento-CENSO2000.xls')
-ibgeEsgoto2000 = ibgeEsgoto2000.parse(0)
 
-ibgeEsgoto2010 = pd.ExcelFile('original_data/IBGE/esgotamento-CENSO2010.xls')
-ibgeEsgoto2010 = ibgeEsgoto2010.parse(0)
-print(ibgeEsgoto2010)
+#========================#
+# Combinacao
+#========================#

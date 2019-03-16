7e50da88cf71d89b1f05ac6c49a1758000933ff8
claudiopacha <claudiomonteiro@protonmail.com>
2019-03-16 20:32:51 -0300

    process data

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -12,12 +12,13 @@ import unidecode
 
 # unaccented_string contains 'Malaga'and is of type 'str
 #===== importar dados =====#
-"""
+
+
 # SNIS
-dataSNIS = pd.ExcelFile('original_data/SNIS_serie_Recife.xlsx').parse(0)
+#dataSNIS = pd.ExcelFile('original_data/SNIS_serie_Recife.xlsx').parse(0)
 
 # Plantas Agamenom
-dataArbAga = pd.ExcelFile('original_data/plantas_agamenom.xls').parse(0)
+#dataArbAga = pd.ExcelFile('original_data/plantas_agamenom.xls').parse(0)
 
 # IBGE Censos 2000 e 2010
 ibgeAgua2000 = pd.ExcelFile('original_data/IBGE/abastecimento_agua-CENSO2000.xls').parse(0)
@@ -25,7 +26,13 @@ ibgeLixo2000 = pd.ExcelFile('original_data/IBGE/destino_lixo-CENSO2000.xls').par
 ibgeAguaLixo2010 = pd.ExcelFile('original_data/IBGE/abastecimento_agua-destino_lixo-CENSO2010.xls').parse(0)
 ibgeEsgoto2000 = pd.ExcelFile('original_data/IBGE/esgotamento-CENSO2000.xls').parse(0)
 ibgeEsgoto2010 = pd.ExcelFile('original_data/IBGE/esgotamento-CENSO2010.xls').parse(0)
-"""
+
+#
+df=ibgeAgua2000
+def numVarName(df)):
+    str_ange = range(0, df.length())
+    
+
 #===========================#
 # Padronizacao
 #===========================#
@@ -39,11 +46,25 @@ def standVarName(frase):
         else:
             std_string += unidecode.unidecode(caracter)
     return std_string.lower()
-    
+
 test = "Caçamba de serviço de limpeza"
 print(standVarName(test))
 
+#----------------------
+# ibgeAgua2000
 
+df = ibgeAgua2000 
+def stdIBGEagua200(df):
+    # capturar nomes das variaveis
+    var_name = ["localidade"]
+    var_name.append(df[])
+
+
+
+
+pd.options.display.max_columns = 50
+pd.options.display.max_rows = 50
+print(ibgeAgua2000) 
 
 #========================#
 # Combinacao
